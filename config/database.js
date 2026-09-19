00b2405867927ea988302a61a99f191ba9642b28
Vishwa <vishwaroobanvr@email.com>
2026-09-19 09:55:47 +0530

    Connect database using environment variable

--- config/database.js
+++ config/database.js
@@ -1,14 +1,20 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    "asset_management",
-    "postgres",
-    "Vishwa0308",
-    {
-        host: "localhost",
-        port: 5432,
+const databaseUrl = process.env.DATABASE_URL;
+
+const sequelize = databaseUrl
+    ? new Sequelize(databaseUrl, {
         dialect: "postgres"
-    }
-);
+    })
+    : new Sequelize(
+        "asset_management",
+        "postgres",
+        "197rUw4KY9xGH6PBCentlU7fvLRKk8Si",
+        {
+            host: "localhost",
+            port: 5432,
+            dialect: "postgres"
+        }
+    );
 
 module.exports = sequelize;
