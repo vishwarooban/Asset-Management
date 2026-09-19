615e6f26110c5b95f602f0bd70512ddce38e7c50
Vishwa <vishwaroobanvr@email.com>
2026-09-19 09:42:12 +0530

    Prepare app for deployment

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ sequelize.authenticate()
         console.log(error.message);
     });
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
