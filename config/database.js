ec8b15d2a91a790eca4af1342870514f884d3181
Vishwa <vishwaroobanvr@email.com>
2026-09-22 16:44:10 +0530

    Update database configuration for Render

--- config/database.js
+++ config/database.js
@@ -1,15 +1,33 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-    "asset_management",
-    "postgres",
-    "Vishwa0308",
-    {
-        host: "localhost",
-        port: 5432,
-        dialect: "postgres",
-        logging: false
-    }
-);
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+
+    sequelize = new Sequelize(
+        process.env.DATABASE_URL,
+        {
+            dialect: "postgres",
+            logging: false
+        }
+    );
+
+} else {
+
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: "postgres",
+            logging: false
+        }
+    );
+
+}
 
 module.exports = sequelize;
